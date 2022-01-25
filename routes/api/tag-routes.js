c90772b79374d8cdc677c0d75674395bc20ef37a
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-25 04:14:59 -0500

    created api routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,89 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll()
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id.' })
+      return;
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  // expects: {"tag_name": "black"}
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  // expects: {"tag_name": "black"}
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id.' })
+      return;
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id.' })
+      return;
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+
 });
 
 module.exports = router;
